9967bc7f23d86f4b6c9cac083d4a0862e459b467
AlexBinkovskyy <alex_bin@ukr.net>
2024-02-22 11:11:44 +0200

    start

---
+++ src/redux/store.js
@@ -0,0 +1,22 @@
+import persistStore from 'redux-persist/es/persistStore';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {},
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistedStore = persistStore(store);
